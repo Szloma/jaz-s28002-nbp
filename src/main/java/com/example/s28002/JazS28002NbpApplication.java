267b98bb65d7fe3dbe6ce5b1fdfddcd372d7c139
Szloma <paprocesor@gmail.com>
2024-06-29 12:45:50 +0200

    Add files via upload

---
+++ src/main/java/com/example/s28002/JazS28002NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.s28002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS28002NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS28002NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s28002/JazS28002NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s28002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS28002NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
